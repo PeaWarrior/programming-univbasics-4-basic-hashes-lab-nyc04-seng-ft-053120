ba97c48e5069798a119a1e1c387ad10cdcb5f66d
Jackson Chen <jacksonchen171@gmail.com>
2020-06-05 15:40:16 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,20 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  financials = {
+    money: 0
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  flatiron = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {id: number}
 end
